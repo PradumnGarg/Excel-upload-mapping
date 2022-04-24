57c3ee1fcac33c159a546756965c71558472ff0a
Pradumn Garg <pradumngarg4050@gmail.com>
2022-04-25 01:46:40 +0530

    Fixed-All columns returned After Z

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const express=require('express');
 const app=express();
 
 //reading from excel file
-const workbook = xlsx.readFile('./test.xlsx'); //excel file name goes here
+const workbook = xlsx.readFile('./items1.xlsx'); //excel file name goes here
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+// console.log(worksheet);
 
 //To get the number of rows
 var range = xlsx.utils.decode_range(worksheet['!ref']);
@@ -29,23 +31,30 @@ if(num_rows>1000){
 }
 else{
 //reading from json fike
-var jsonObject=JSON.parse(fs.readFileSync('excel1.json', 'utf8')); //json config file name goes here
+var jsonObject=JSON.parse(fs.readFileSync('excel.json', 'utf8')); //json config file name goes here
+// console.log(jsonObject);
 // var keys = Object.keys( jsonObject );
 
 
 //looping through each cell in the excel file
 for (let cell in worksheet) {
+ checkAt=1; //to ensure columns like AA are also considered
 
+    // console.log(cell);
     const cellAsString = cell.toString();
-// console.log(jsonObject[cellAsString[0]]);
 
-    if (cellAsString[1] !== 'r' && cellAsString[1] !== 'm' && cellAsString[1] > 1) {
+if(cellAsString.length>2){
+    checkAt=2;
+}
 
-            post[jsonObject[cellAsString[0]]] = worksheet[cell].v;
 
-        
+    if (cellAsString[1] !== 'r' && cellAsString[1] !== 'm' && cellAsString[checkAt] > 1) {
+              
+         
+            post[jsonObject[cellAsString.substring(0,checkAt)]] = worksheet[cell].v;
+
         //checks for last column so as to push into the array
-        if (cellAsString[0] === Object.keys(jsonObject).sort().reverse()[0]) {
+        if (cellAsString.substring(0,checkAt) === Object.keys(jsonObject).reverse()[0]) {
             posts.push(post);
             post = {};
         }
